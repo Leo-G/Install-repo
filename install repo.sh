0b54895e9cedba6f449c5d1767fe6c83dee51a1b
Leo <leo@leog.in>
2013-11-03 22:49:25 +0530

    script

---
+++ install repo.sh
@@ -0,0 +1,19 @@
+ #!/bin/bash
+ #Author Leo G
+ # This script will install both Atomic and epel repos, report any errors to leo@leog.in
+ # Copyright 2013 Leonard Gonsalves leog.in
+ #This program is free software: you can redistribute it and/or modify
+ #   it under the terms of the GNU General Public License as published by
+ #   the Free Software Foundation, either version 3 of the License, or
+ #   (at your option) any later version.
+ #
+ #   This program is distributed in the hope that it will be useful,
+ #   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ #   GNU General Public License for more details.
+ # 
+ #   You should have received a copy of the GNU General Public License
+ #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ wget -q -O - http://www.atomicorp.com/installers/atomic | sh
+ rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
